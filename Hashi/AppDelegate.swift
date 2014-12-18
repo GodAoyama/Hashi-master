5ed7c55a0361cd0067afa7ad2848e487ae56c676
maedaken <shack@shack-no-MacBook-Air.local>
2014-12-18 14:05:27 +0900

    HashiCentral　getInt16FromRawを変更
    1byteずつもってきて先頭を8bitずらしたあと
    のこりの8bitを足す

---
+++ Hashi/AppDelegate.swift
@@ -0,0 +1,114 @@
+//
+//  AppDelegate.swift
+//  Hashi
+//
+//  Created by Keichi Takahashi on 12/6/14.
+//  Copyright (c) 2014 Keichi Takahashi. All rights reserved.
+//
+
+import Cocoa
+import CoreBluetooth
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, HashiCentralDelegate {
+    @IBOutlet weak var window: NSWindow!
+    
+    @IBOutlet weak var lblAccX: NSTextField!
+    @IBOutlet weak var lblAccY: NSTextField!
+    @IBOutlet weak var lblAccZ: NSTextField!
+    
+    @IBOutlet weak var lblRotX: NSTextField!
+    @IBOutlet weak var lblRotY: NSTextField!
+    @IBOutlet weak var lblRotZ: NSTextField!
+    
+    @IBOutlet weak var lblConnectionStatus: NSTextField!
+    @IBOutlet weak var lblSampleCount: NSTextField!
+    @IBOutlet weak var txtOutputPath: NSTextField!
+    
+    @IBOutlet weak var btnRecord: NSButton!
+    @IBOutlet weak var btnStop: NSButton!
+    
+    var recordFlag = false
+    
+    var hashiCentral: HashiCentral!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        hashiCentral = HashiCentral()
+        hashiCentral.delegate = self
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+    func didConnectToPeripheral(peripheral: CBPeripheral, error: NSError!) {
+        lblConnectionStatus.stringValue = "Connected"
+        lblConnectionStatus.textColor = NSColor.greenColor()
+    }
+    
+    func didDisconnectFromPeripheral(peripheral: CBPeripheral, error: NSError!) {
+        lblConnectionStatus.stringValue = "Disconnected"
+        lblConnectionStatus.textColor = NSColor.redColor()
+    }
+    
+    private func formatFloatValue(value: Float) -> String {
+        return NSString(format: "%.3f", value)
+    }
+    
+    func didUpdateAcceleration(accX: Float, accY: Float, accZ: Float) {
+        var x = NSString(format: "%.3f g", accX)
+        var y = NSString(format: "%.3f g", accY)
+        var z = NSString(format: "%.3f g", accZ)
+        
+        lblAccX.stringValue = NSString(format: "%.3f g", accX)
+        lblAccY.stringValue = NSString(format: "%.3f g", accY)
+        lblAccZ.stringValue = NSString(format: "%.3f g", accZ)
+        
+        if(recordFlag == true){
+            // 実際の保存
+            var fileObj:NSString = ""
+            fileObj = fileObj + x + "," + y + "," + z + "¥n"
+            // fileObj.writeToFile(<#path: String#>, atomically: <#Bool#>, encoding: <#NSStringEncoding#>, error: <#NSErrorPointer#>)
+            fileObj.writeToFile("/User/shack/Desktop/Hashi/hashi.log", atomically: false, encoding: NSUTF8StringEncoding, error: nil)
+            
+            recordFlag=false;
+        }
+    }
+    
+    func didUpdateRotationRate(rotX: Float, rotY: Float, rotZ: Float) {
+        lblRotX.stringValue = NSString(format: "%.3f deg/s", rotX)
+        lblRotY.stringValue = NSString(format: "%.3f deg/s", rotY)
+        lblRotZ.stringValue = NSString(format: "%.3f deg/s", rotZ)
+    }
+    
+    @IBAction func btnRecordPressed(sender: NSButton) {
+        btnRecord.enabled = false
+        btnStop.enabled = true
+        recordFlag = true
+        
+    }
+    
+    @IBAction func btnStopPressed(sender: NSButton) {
+        btnRecord.enabled = true
+        btnStop.enabled = false
+        recordFlag = false
+    }
+    
+    @IBAction func btnSelectOutputPressed(sender: NSButton) {
+        let dialog = NSOpenPanel()
+        dialog.canChooseDirectories = true
+        dialog.canChooseFiles = false
+        dialog.canCreateDirectories = true
+        dialog.allowsMultipleSelection = false
+        dialog.worksWhenModal = true
+        dialog.prompt = "Select"
+        
+        if (dialog.runModal() == NSOKButton) {
+            if let url = dialog.directoryURL {
+                txtOutputPath.stringValue = url.path!
+            }
+        }
+    }
+}
+
---
+++ Hashi/HashiCentral.swift
@@ -0,0 +1,155 @@
+//
+//  HashiCentral.swift
+//  Hashi
+//
+//  Created by Keichi Takahashi on 12/6/14.
+//  Copyright (c) 2014 Keichi Takahashi. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class HashiCentral : NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    private let hashiServiceUUID = CBUUID(string: "312599d0-6450-4d14-8c4f-eb89d66d6d8d")
+    private let accelCharUUID = CBUUID(string: "e715d934-c6cf-4999-b82d-6262fadd0152")
+    private let gyroCharUUID = CBUUID(string: "42de3770-40cb-4ff0-8e32-13f621fba635")
+
+    private var centralManager: CBCentralManager!
+    private var peripheral: CBPeripheral!
+    
+    weak var delegate: HashiCentralDelegate!
+    
+    override init() {
+        super.init()
+        
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        println("Connected to peripheral \(peripheral.name)")
+        
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didDisconnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+        delegate?.didDisconnectFromPeripheral(peripheral, error: error)
+    }
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        println("Discovered peripheral: \(peripheral)")
+        println("Advertisement data: \(advertisementData)")
+        println("RSSI: \(RSSI)")
+        
+        self.peripheral = peripheral
+        central.stopScan()
+        central.connectPeripheral(peripheral, options: nil)
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        switch central.state {
+        case .PoweredOn:
+            println("CBCentralManager is powered on")
+            centralManager.scanForPeripheralsWithServices([hashiServiceUUID], options: nil)
+            break
+        case .PoweredOff:
+            println("CBCentralManager is powered off")
+            break
+        case .Resetting:
+            println("CBCentralManager is resetting")
+            break
+        case .Unauthorized:
+            println("This application is not allowed to use BLE")
+            break
+        case .Unknown:
+            println("CBCentralManager state is unknwon")
+            break
+        case .Unsupported:
+            println("This device does not support BLE")
+            break
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        if error != nil {
+            delegate?.didConnectToPeripheral(peripheral, error: error)
+            return
+        }
+        if peripheral.services.count == 0 {
+            let err = NSError(domain: "net.keichi.HashiCentral.NoService", code: -1, userInfo: [NSLocalizedDescriptionKey: "No services are discovered"])
+            delegate?.didConnectToPeripheral(peripheral, error: err)
+            return
+        }
+        
+        for service in peripheral.services as [CBService] {
+            println("Discovered service \(service.UUID.description)")
+            
+            peripheral.discoverCharacteristics([accelCharUUID, gyroCharUUID], forService: service)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        if error != nil {
+            delegate?.didConnectToPeripheral(peripheral, error: error)
+            return
+        }
+        if service.characteristics.count == 0 {
+            let err = NSError(domain: "net.keichi.HashiCentral.NoCharacteristic", code: -1, userInfo: [NSLocalizedDescriptionKey: "No characteristics are discovered"])
+            delegate?.didConnectToPeripheral(peripheral, error: err)
+            return
+        }
+        
+        for characteristic in service.characteristics as [CBCharacteristic] {
+            println("Discovered characteristic \(characteristic.UUID.description)")
+            
+            if (characteristic.UUID.UUIDString == accelCharUUID.UUIDString) {
+                println("Accelerometer characteristic found, enabling notification")
+                peripheral.setNotifyValue(true, forCharacteristic: characteristic)
+            } else if (characteristic.UUID.UUIDString == gyroCharUUID.UUIDString) {
+                println("Gyrometer characteristic found, enabling notification")
+                peripheral.setNotifyValue(true, forCharacteristic: characteristic)
+            }
+            
+            delegate?.didConnectToPeripheral(peripheral, error: nil)
+        }
+    }
+    
+    private func getInt16FromRaw(data: NSData, offset: Int) -> Int16 {
+        var val: Int16 = 0
+        var val2: Int16 = 0
+        
+        data.getBytes(&val, range: NSRange(location: offset, length: 1))
+        data.getBytes(&val2, range: NSRange(location: offset+1, length: 1))
+        
+        val = val << 8
+        val = val + val2
+        
+        return val
+    }
+    
+    private func getAccelerationFromRaw(data: NSData) -> (Float, Float, Float) {
+        let accX = 2 * Float(getInt16FromRaw(data, offset: 0)) / 32768
+        let accY = 2 * Float(getInt16FromRaw(data, offset: 2)) / 32768
+        let accZ = 2 * Float(getInt16FromRaw(data, offset: 4)) / 32768
+        
+        return (accX, accY, accZ)
+    }
+    
+    private func getRotationRateFromRaw(data: NSData) -> (Float, Float, Float) {
+        let rotX = 250 * Float(getInt16FromRaw(data, offset: 0)) / 32768
+        let rotY = 250 * Float(getInt16FromRaw(data, offset: 2)) / 32768
+        let rotZ = 250 * Float(getInt16FromRaw(data, offset: 4)) / 32768
+        
+        return (rotX, rotY, rotZ)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
+        if (characteristic.UUID.UUIDString == accelCharUUID.UUIDString) {
+            let (accX, accY, accZ) = getAccelerationFromRaw(characteristic.value())
+            delegate?.didUpdateAcceleration(accX, accY: accY, accZ: accZ)
+        } else if (characteristic.UUID.UUIDString == gyroCharUUID.UUIDString) {
+            let (rotX, rotY, rotZ) = getRotationRateFromRaw(characteristic.value())
+            delegate?.didUpdateRotationRate(rotX, rotY: rotY, rotZ: rotZ)
+        }
+    }
+}
